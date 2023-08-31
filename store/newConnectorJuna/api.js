2e02ecd93b778a8810a4e36541a8c0fcad25812a
Crowdbotics <engineering@crowdbotics.com>
2023-08-31 13:20:30 +0000

    Update store.

---
+++ store/newConnectorJuna/api.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+const newConnectorJuna = axios.create({
+  baseURL: "http://gggl.com",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
