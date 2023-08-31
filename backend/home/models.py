6e3ac2fcc3bef03e68eab59254e43c6d6412b1f8
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-31 14:22:41 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,9 @@
+from django.conf import settings
 from django.db import models
+class Azul(models.Model):
+    'Generated Model'
+    acf = models.BigIntegerField()
+    asdc = models.BigIntegerField()
+    acvvv = models.BigIntegerField()
+    avbnsdbf = models.BigIntegerField()
+    sdf = models.BigIntegerField()
